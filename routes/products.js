8772a8e285102ab0f325521e6b54db88cce011b1
mohammadAminSH1524 <harrywill1524@mailfa.com>
2023-05-14 22:25:04 +0430

    data base created with node.js

---
+++ routes/products.js
@@ -0,0 +1,17 @@
+const userRoutes = (app, fs) => {
+    // variables
+    const dataPath = './data/products.json';
+  
+    // READ
+    app.get('/products', (req, res) => {
+      fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+          throw err;
+        }
+  
+        res.send(JSON.parse(data));
+      });
+    });
+  };
+  
+  module.exports = userRoutes;
